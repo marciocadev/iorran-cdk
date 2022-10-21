df70f47cde600c24ca75ddfc8c16db9cd0e21c57
Marcio Almeida <marciocadev@gmail.com>
2022-10-21 07:59:17 -0300

    Initial commit

---
+++ test/iorran-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IorranCdk from '../lib/iorran-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iorran-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IorranCdk.IorranCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
